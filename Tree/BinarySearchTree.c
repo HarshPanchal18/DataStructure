600d73687364c1e0bf8a998fb0b2afd534a6b0fb
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2022-03-10 11:44:26 +0530

    Create BinarySearchTree.c

---
+++ Tree/BinarySearchTree.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/*
+Binary Search Tree: A binary tree in which for each node, value of all the nodes in left-subtree is lesser or equal
+                        and value of all the nodes in right-subtree is greater.
+*/
+
+/*Tree Structure
+
+           |  ROOT  |
+            /      \
+      |<=ROOT|   |>ROOT|
+*/
