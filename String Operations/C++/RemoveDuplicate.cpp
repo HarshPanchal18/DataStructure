aa81203cfb6e508617d081f6974250bea63ae893
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2022-03-11 18:02:30 +0530

    Added one more function

--- String Operations/C++/RemoveDuplicate.cpp
+++ String Operations/C++/RemoveDuplicate.cpp
@@ -87,6 +87,31 @@ string remove3(string str)
     return str;
 }
 
+void remove4(char *str)
+{
+    /*
+    Time Complexity: O(n)
+    Return: void, change the char array we pass as arg
+    */
+
+    int len = strlen(str);
+    if (len < 2)
+        return;
+
+    int check = 0, Cindex = 0;
+
+    for (int i = 0; i < len; i++)
+    {
+        int c = (int)(str[i] - 'a');
+        if ((check & (1 << c)) == 0)
+        {
+            str[Cindex++] = str[i];
+            check |= (1 << c);
+        }
+    }
+    str[Cindex] = '\0';
+}
+
 int main(void)
 {
     string s1("abbec");
@@ -111,5 +136,27 @@ int main(void)
     cout << "Original: " << s2 << " Removing: " << remove3(s2) << endl;
     cout << "Original: " << s3 << " Removing: " << remove3(s3) << endl;
     cout << "Original: " << s4 << " Removing: " << remove3(s4) << endl;
+    
+    cout << "\nFor Method 4: " << endl;
+    char s5[] = "aaaabbbccccc";
+    char s6[] = "abcabcabc";
+    char s7[] = "a";
+    char s8[] = "";
+
+    cout << "Original: " << s5 << " Removing: ";
+    remove4(s5);
+    cout << s5 << endl;
+
+    cout << "Original: " << s6 << " Removing: ";
+    remove4(s6);
+    cout << s6 << endl;
+
+    cout << "Original: " << s7 << " Removing: ";
+    remove4(s7);
+    cout << s7 << endl;
+
+    cout << "Original: " << s8 << " Removing: ";
+    remove4(s8);
+    cout << s8 << endl;
 
 }
