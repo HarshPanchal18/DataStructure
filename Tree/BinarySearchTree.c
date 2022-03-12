44c616ed081ca88e22fd0c16f3bd7193c36d3d7a
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2022-03-12 10:41:47 +0530

    Update BinarySearchTree.c

--- Tree/BinarySearchTree.c
+++ Tree/BinarySearchTree.c
@@ -13,3 +13,18 @@ Binary Search Tree: A binary tree in which for each node, value of all the nodes
             /      \
       |<=ROOT|   |>ROOT|
 */
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+struct Node *GetNewNode(int data)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
