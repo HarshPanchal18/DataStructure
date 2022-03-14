4afd33b427515dc82a0d95f12819eb6e94e9585d
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2022-03-14 16:15:26 +0530

    Update BSTree.C

--- Array Operations/C/BSTree.C
+++ Array Operations/C/BSTree.C
@@ -15,3 +15,26 @@ N *new (int x)
     node->r = NULL;
     return node;
 }
+
+N *BST(int arr[], int first, int last)
+{
+    if (first > last)
+        return NULL;
+
+    N *temp = (N *)malloc(sizeof(N));
+    int mid = (first + last) / 2;
+
+    temp = new (arr[mid]);
+    temp->l = BST(arr, first, mid - 1);
+    temp->r = BST(arr, mid + 1, last);
+    return temp;
+}
+
+void Display(N *temp)
+{
+    printf("%d ", temp->data);
+    if (temp->l != NULL)
+        Display(temp->l);
+    if (temp->r != NULL)
+        Display(temp->r);
+}
