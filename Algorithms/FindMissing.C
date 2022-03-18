5908db27ebc31911fdc8406e25e0b1cb37d6dcc0
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2022-03-18 11:31:36 +0530

    Create FindMissing.C

---
+++ Algorithms/FindMissing.C
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    printf("Enter the array:\n");
+    int *a = (int *)malloc(n * sizeof(int));
+    for (int i = 1; i <= n; ++i)
+        scanf("%d", &a[i]);
+
+    if (a == NULL)
+    {
+        printf("Memory not allocated.\n");
+        return 0;
+    }
+    else
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            if (a[i] <= 0 || a[i] > n)
+                continue;
+
+            int x = a[i];
+            while (a[x] != x)
+            {
+                int next_ele = a[x];
+                a[x] = x;
+                x = next_ele;
+                if (x <= 0 || x > n)
+                    break;
+            }
+        }
+        int ans = n + 1;
+        for (int i = 1; i <= n; i++)
+        {
+            if (a[i] != i)
+            {
+                ans = i;
+                break;
+            }
+        }
+        printf("%d", ans);
+    }
+
+    return 0;
+}
+
+/*
+Time Complexity: O(N)
+Space Complexity: O(1)
+OUTPUT
+Enter the value of n: 4
+Enter the array:
+3 4 -1 1
+2
+*/
