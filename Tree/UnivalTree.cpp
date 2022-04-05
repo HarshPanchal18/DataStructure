1c2c11d1a54c7ddc33aff0868d7699d108e65a0a
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2022-04-05 14:59:45 +0530

    Update UnivalTree.cpp

--- Tree/UnivalTree.cpp
+++ Tree/UnivalTree.cpp
@@ -1,3 +1,17 @@
+/*
+A unival tree (which stands for "universal value") is a tree where all nodes under it have the same value.
+Given the root to a binary tree, count the number of unival subtrees.
+For example, the following tree has 5 unival subtrees:
+   0
+  / \
+ 1   0
+    / \
+   1   0
+  / \
+ 1   1
+ */
+
+
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -29,4 +43,19 @@ int unival(Tree *node, int data)
         return min(x, 1);
     else
         return 0;
+}
+
+
+int main(void)
+{
+    ans = 0;
+    Tree *root = new Tree(0);
+    root->left = new Tree(1);
+    root->right = new Tree(0);
+    root->right->right = new Tree(0);
+    root->right->left = new Tree(1);
+    root->right->left->left = new Tree(1);
+    root->right->left->right = new Tree(1);
+    unival(root, -1);
+    cout << ans << endl;
 }
