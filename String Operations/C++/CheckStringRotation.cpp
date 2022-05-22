edcf4842186e7ae6dcc7fe24e42f8715c49d1bc4
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2022-05-22 10:29:04 +0530

    Create CheckStringRotation.cpp

---
+++ String Operations/C++/CheckStringRotation.cpp
@@ -0,0 +1,30 @@
+/*Suppose there are two strings s1 = "devincept" and s2 = "vinceptde",
+to check if s2 is rotation of s1, what we can do is, add s1 to itself
+Assign it to another string s3 = s1 + s1
+If s2 is a substring of s1, it is rotation of s1.
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string s1, s2, s3;
+    cout << "Enter the strings: " << endl;
+    cin >> s1;
+    cin >> s2;
+
+    s3 = s1 + s1;
+
+    if (s3.find(s2) != -1 && s1 != s2) // if s2 is not a substring of s1 then find returns the -1
+        cout << s2 << " is rotation of " << s1;
+    else
+        cout << s2 << " is not rotation of " << s1;
+    return 0;
+}
+/*
+Enter the strings:
+rotation
+ationrot
+ationrot is rotation of rotation
+*/
