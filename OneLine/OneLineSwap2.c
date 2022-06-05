a16b2fca252b2cdc1c62ea07a0a4ba2483e9d4c1
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2022-06-05 11:32:40 +0530

    Create OneLineSwap2.c

---
+++ OneLine/OneLineSwap2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define swap(x, y) ((x ^= y), (y ^= x), (x ^= y))
+
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("Before swap: %d, %d\n", a, b);
+
+    swap(a, b);
+
+    printf("After swap: %d, %d\n", a, b);
+}
