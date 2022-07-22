5f325b38e314f52da73e51ffd4b4d4d3e10afa25
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2022-07-22 11:55:27 +0530

    Create rawExtension.c

---
+++ String Operations/C/rawExtension.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    int i;
+    char *s;
+
+    for (i = 1; i < argc; i++)
+    {
+        if (!(s = strrchr(argv[i], '.')))
+            puts(argv[i]);
+        else
+        {
+            *s = '\0';
+            puts(s + 1);
+        }
+    }
+    return 0;
+}
+/*
+.\rawExtension 1.c 2.cpp th.py
+c
+cpp
+py
+*/
