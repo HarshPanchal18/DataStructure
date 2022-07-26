82500fda074005ef3067f8b2a84db3dfe819bf6b
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2022-07-26 10:40:27 +0530

    Create repeatLine.c

---
+++ String Operations/C/repeatLine.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int i, l = argc - 1;
+
+    if (argc < 2)
+        return 0;
+
+    for (;;)
+    {
+        for (i = 1; i < l; i++)
+        {
+            fputs(argv[i], stdout);
+            putchar(' ');
+        }
+        puts(argv[l]);
+    }
+}
