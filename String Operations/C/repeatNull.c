2d8dfc9dcbe5efe0a11749ed5b2179dd23d97ee5
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2022-09-13 11:22:30 +0530

    Create repeatNull.c

---
+++ String Operations/C/repeatNull.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int i, l = argc - 1;
+
+    if (argc < 2)
+        return 0;
+
+    for (;;)
+    {
+        for (i = 1; i < l; i++)
+        {
+            fputs(argv[i], stdout);
+            putchar(' ');
+        }
+        fputs(argv[l], stdout);
+        putchar('\0');
+    }
+}
