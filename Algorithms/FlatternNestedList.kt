5b21298d9bed57dbeea061dbf21bc2131ba8372c
Harsh Panchal <harshhhh1803@gmail.com>
2023-10-22 11:00:54 +0530

    Create FlatternNestedList.kt

---
+++ Algorithms/FlatternNestedList.kt
@@ -0,0 +1,93 @@
+/*
+* Flatten Nested List Iterator
+You are given a nested list of integers nestedList.
+Each element is either an integer or a list whose elements may also be integers or other lists.
+Implement an iterator to flatten it.
+
+Implement the NestedIterator class:
+NestedIterator(List<NestedInteger> nestedList) Initializes the iterator with the nested list nestedList.
+int next() Returns the next integer in the nested list.
+boolean hasNext() Returns true if there are still some integers in the nested list and false otherwise.
+Your code will be tested with the following pseudocode:
+
+initialize iterator with nestedList
+res = []
+while iterator.hasNext()
+    append iterator.next() to the end of res
+return res
+
+If res matches the expected flattened list, then your code will be judged as correct.
+
+Example 1:
+Input: nestedList = [[1,1],2,[1,1]]
+Output: [1,1,2,1,1]
+Explanation: By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,1,2,1,1].
+
+Example 2:
+Input: nestedList = [1,[4,[6]]]
+Output: [1,4,6]
+Explanation: By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,4,6].
+*/
+
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *     // Constructor initializes an empty nested list.
+ *     constructor()
+ *
+ *     // Constructor initializes a single integer.
+ *     constructor(value: Int)
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     fun isInteger(): Boolean
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     fun getInteger(): Int?
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     fun setInteger(value: Int): Unit
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     fun add(ni: NestedInteger): Unit
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     fun getList(): List<NestedInteger>?
+ * }
+ */
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * var obj = NestedIterator(nestedList)
+ * var param_1 = obj.next()
+ * var param_2 = obj.hasNext()
+ */
+
+ class NestedIterator(nestedList: List<NestedInteger>) {
+    val list = mutableListOf<Int>()
+    var size = 0
+    var index = 0
+
+    init { helper(nestedList) }
+
+    fun helper(nestedList: List<NestedInteger>) {
+        for (i in 0..nestedList.size-1) {
+            val integer = nestedList[i]
+
+            if(integer.isInteger())
+                list.add(integer.getInteger())
+            else
+                helper(integer.getList())
+        }
+        size = list.size
+    }
+
+    fun next(): Int {
+        return list[index++]
+    }
+
+    fun hasNext(): Boolean {
+        return index < size
+    }
+}
