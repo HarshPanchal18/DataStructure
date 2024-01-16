b89976ec451dbc68634bab3a99fb8ab84b572776
Harsh Panchal <harshhhh1803@gmail.com>
2024-01-16 12:22:52 +0530

    Create LongestsPalindromeLength.java

---
+++ String Operations/Kotlin/LongestPalindromeLength.java
@@ -0,0 +1,46 @@
+/*
+ * Longest Palindromic Substring
+
+Given a string s, return the longest palindromic substring in s.
+A string is called a palindrome string if the reverse of that string is the same as the original string.
+
+Example 1:
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+
+Example 2:
+Input: s = "cbbd"
+Output: "bb"
+*/
+
+public class LongestPalindromeLength {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) {
+            return "";
+        }
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println((new LongestPalindromeLength()).longestPalindrome("babad")); // bab
+    }
+}
