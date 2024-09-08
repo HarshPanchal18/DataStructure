13d5ce92afd4f1cc0b5b1c49768f07927cc62d03
Harsh Panchal <harshhhh1803@gmail.com>
2024-09-08 13:15:04 +0530

    Create RemoveElement.kt

---
+++ Array Operations/Kotlin/RemoveElement.kt
@@ -0,0 +1,114 @@
+/*
+* Remove Element - https://leetcode.com/problems/remove-element/
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place.
+The order of the elements may be changed.
+Then return the number of elements in nums which are not equal to val.
+
+Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+* Change the array nums such that the first k elements of nums contain the elements which are not equal to val.
+* The remaining elements of nums are not important as well as the size of nums.
+* Return k.
+
+* Custom Judge:
+The judge will test your solution with the following code:
+int[] nums = [...]; // Input array
+int val = ...; // Value to remove
+int[] expectedNums = [...]; // The expected answer with correct length.
+                            // It is sorted with no values equaling val.
+
+int k = removeElement(nums, val); // Calls your implementation
+
+assert k == expectedNums.length;
+sort(nums, 0, k); // Sort the first k elements of nums
+for (int i = 0; i < actualLength; i++) {
+    assert nums[i] == expectedNums[i];
+}
+If all assertions pass, then your solution will be accepted.
+
+Example 1:
+Input: nums = [3,2,2,3], val = 3
+Output: 2, nums = [2,2,_,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+Example 2:
+Input: nums = [0,1,2,2,3,0,4,2], val = 2
+Output: 5, nums = [0,1,4,0,3,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
+Note that the five elements can be returned in any order.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+Constraints:
+0 <= nums.length <= 100
+0 <= nums[i] <= 50
+0 <= val <= 100
+*/
+
+fun main() {
+	println(removeElement(intArrayOf(3,2,2,3), 3))
+}
+
+fun removeElement(nums: IntArray, `val`: Int): Int {
+	var count = 0
+	var start = 0
+	var end = nums.lastIndex
+
+	while (start <= end) {
+		if (nums[start] == `val`) {
+			count++ // 0, 0, 1
+
+			// 0,1,2,2,3,0,4,2 | 2
+			//   s			 e
+			// Swap elements
+			nums[start] = nums[end].also { nums[end] = nums[start] }
+			end--
+		} else {
+			start++
+		}
+	}
+
+	return nums.size - count
+}
+
+class Solution {
+	fun removeElement(nums: IntArray, `val`: Int): Int {
+		var k = 0
+
+		for (i in nums.indices) {
+			if (nums[i] != `val`) {
+				nums[k] = nums[i]
+				k++
+			}
+		}
+		return k
+	}
+}
+
+class Solution {
+	fun removeElement(nums: IntArray, num: Int): Int {
+		var counter = 0
+		nums.forEach {
+			if (it != num)
+				nums[counter++] = it
+		}
+		return counter
+	}
+}
+
+class Solution {
+	fun removeElement(nums: IntArray, value: Int): Int {
+		if (nums.isEmpty())
+			return 0
+
+		var slow = 0
+
+		for (fast in nums.indices) {
+			if (nums[fast] != value) {
+				nums[slow] = nums[fast]
+				slow++
+			}
+		}
+
+		return slow
+	}
+}
